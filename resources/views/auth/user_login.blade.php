23fced1ea144c376f72fed6ab304a736f33b08c1
sunil <he.kingsmen@gmail.com>
2024-07-24 14:50:36 +0530

    Sign Up

--- resources/views/auth/user_login.blade.php
+++ resources/views/auth/user_login.blade.php
@@ -13,27 +13,34 @@
 
         <div class="col-lg-6" style="position: relative;">
           <h1 class="heading login">Welcome</h1>
-          <div class="row welcome-log-in">
-            <div class="col-lg-12">
 
-              <div class="mb-3 d-flex" style="gap: 15px;">
-                <P style="margin-bottom: 0.5rem;">IND</P>
-                <label for="exampleFormControlInput1" class="form-label">Phone Number</label>
+          <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div class="row welcome-log-in">
+              <div class="col-lg-12">
+
+                <div class="mb-3 d-flex" style="gap: 15px;">
+                  <P style="margin-bottom: 0.5rem;">IND</P>
+                  <label for="exampleFormControlInput1" class="form-label">Phone Number</label>
+                </div>
+
+                <div class="mb-3 d-flex" style="gap: 15px;">
+                  <p style="margin-bottom: 0 !important;padding: 7px;background: #fff;border: 1px solid #ccc;border-radius: 10px;"> +91 </p>
+                  <div class="input-container">
+                    <x-text-input class="form-control ph-no" id="exampleFormControlInput1" type="number" name="username" :value="old('username')" placeholder="9744448548252" required autofocus />
+                    <x-input-error :messages="$errors->get('username')" class="mt-2" />
+                  </div>  
+                </div>
+
               </div>
-
-              <div class="mb-3 d-flex" style="gap: 15px;">
-                <p style="margin-bottom: 0 !important;padding: 7px;background: #fff;border: 1px solid #ccc;border-radius: 10px;"> +91 </p>
-                <div class="input-container">
-                  <input type="number" class="form-control ph-no" id="exampleFormControlInput1" placeholder="9744448548252">
-                </div>  
-              </div>
-
             </div>
-          </div>
 
-          <div class="log-in-button">
-            <a href="" class="btn4" data-bs-toggle="modal" data-bs-target="#exampleModal2">Login</a>
-          </div>
+            <div class="log-in-button">
+              <a href="" class="btn4" data-bs-toggle="modal" data-bs-target="#exampleModal2">Login</a>
+            </div>
+
+          </form>
 
           <p class="para">Mobile Number Not Registered ? <a href="" class="register" data-bs-toggle="modal" data-bs-target="#exampleModal"> Register now</a> </p>
           <img src="{{ asset('assets/theme/image/welcome page bottom image.png') }}" alt="" class="img-fluid bottom-img">
@@ -44,6 +51,257 @@
 
         </div>
 
+      </div>
+    </div>
+  </div>
+
+
+
+    <!-- modal Create Profile -->
+    <div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Create Profile</h5>
+            <a href="" data-bs-dismiss="modal" aria-label="Close">
+              <img src="./image/cross.png" alt="">
+            </a>
+          </div>
+          <div class="modal-body">
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <h2 class="personal-heading">Personal Information </h2>
+                <div class="row" style="background-color: #fff !important;">
+                  
+                <div class="col-lg-6">
+                    <div class="mb-3">
+                      <x-input-label for="exampleFormControlInput1" class="form-label" :value="__('Name')" />
+                      <div class="input-container input-container1">
+                        <x-text-input class="form-control" id="exampleFormControlInput1" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Pritam Ghosh" />
+                      </div>
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <x-input-label for="exampleFormControlInput1" class="form-label" :value="__('Email Address')" />
+                      <div class="input-container input-container2">
+                        <x-text-input class="form-control" id="exampleFormControlInput1" type="email" name="email" :value="old('email')" required autocomplete="email" placeholder="Example@gmail.com" />
+                      </div>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                  </div>
+                </div>
+
+
+                <h2 class="personal-heading">Contact Information </h2>
+
+                <div class="row" style="background-color: #fff !important;">
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Phone Number</label>
+                      <div class="input-container">
+                        <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="7776776877">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label" style="visibility: hidden;">Emailid</label>
+                      <a href="">
+                        <div class="button-otp">
+                          Send OTP
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div class="otp mb-3">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <p style="color: #FF0000;">OTP Expires in: <span style="color: #00744A;"> 01:51</span></p>
+                    </div>
+                    <div class="col-lg-6 d-flex justify-content-end">
+                      <img src="./image/cross.png" alt="" class="img-fluid" style="width: 18px;height: 18px;">
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="mb-3">
+                        <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Enter OTP">
+                      </div>
+                    </div>
+                    <div class="col-lg-4 d-flex" style="gap: 15px;">
+                      <div class="mb-3">
+                        <a href="">
+                          <div class="button-otp">
+                            Submit OTP
+                          </div>
+                        </a>
+                      </div>
+                      <div class="mb-3">
+                        <a href="">
+                          <div class="button-otp" style="background: #FFC700;">
+                            Resend OTP
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div class="otp">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <p style="color: #FF0000;">OTP Expires in: <span style="color: #00744A;"> 01:51</span></p>
+                    </div>
+                    <div class="col-lg-6 d-flex justify-content-end">
+                      <img src="./image/cross.png" alt="" class="img-fluid" style="width: 18px;height: 18px;">
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="mb-3">
+                        <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Enter OTP">
+                      </div>
+                    </div>
+                    <div class="col-lg-4 d-flex" style="gap: 15px;">
+                      <div class="mb-3">
+                        <div class="button-otp">
+                          <a href="">Submit OTP</a>
+                        </div>
+                      </div>
+                      <div class="mb-3">
+                        <div class="button-otp" style="background: #FFC700;">
+                          <a href="">Resend OTP</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+                <h2 class="personal-heading">Others</h2>
+
+                <div class="row" style="background-color: #fff !important;">
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Date Of Birth</label>
+                      <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="24/04/2001">
+                    </div>
+                  </div>
+                  <div class="col-lg-6"></div>
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Flat,House No, Building,Company</label>
+                      <div class="input-container input-container3">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mumbai">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Area , Street, Sector, Village</label>
+                      <div class="input-container input-container3">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mumbai">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Landmark</label>
+                      <div class="input-container input-container3">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mumbai">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Town/City</label>
+                      <div class="input-container input-container3">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mumbai">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">State</label>
+                      <div class="input-container input-container3">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mumbai">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Pin Code</label>
+                      <div class="input-container1">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Pin Code">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+                <p class="modal-bottom-text">By creating an account , You agree to the <a href=""
+                    style="color: #08AF73;">Terms of service</a> & <a href="" style="color: #08AF73;">Privacy Policy</a>.
+                </p>
+
+            </form>
+          </div>
+          <div class="modal-footer justify-content-center" style="padding-top: 0;">
+            <a href="">
+              <div class="button-otp">
+                Save
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <!-- modal 2 Confirm OTP -->
+  <div class="modal" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Confirm OTP</h5>
+          <a href="" data-bs-dismiss="modal" aria-label="Close">
+            <img src="./image/cross.png" alt="">
+          </a>
+        </div>
+        <div class="modal-body">
+          <p style="font-size: 20px;">Please enter OTP , sent on your mobile number</p>
+          <form action="">
+            <div class="otp mb-3">
+              <div class="row">
+                <div class="col-lg-12">
+                  <p style="color: #FF0000;">OTP Expires in: <span style="color: #00744A;"> 01:51</span></p>
+                </div>
+                <div class="col-lg-6">
+                  <div class="mb-3">
+                    <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Enter OTP">
+                  </div>
+                </div>
+                <div class="col-lg-4 d-flex" style="gap: 15px;">
+                  <div class="mb-3">
+                    <a href="Complainant.html">
+                      <div class="button-otp">
+                        Submit OTP
+                      </div>
+                    </a>
+                  </div>
+                  <div class="mb-3">
+                    <a href="">
+                      <div class="button-otp" style="background: #FFC700;">
+                        Resend OTP
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </form>
+        </div>
       </div>
     </div>
   </div>
